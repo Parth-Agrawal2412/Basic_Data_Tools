a53d5df100aecb263f39a7ae8496a7ba8f739045
Parth Agrawal <parthagrawal2412@gmail.com>
2026-09-20 18:18:52 +0530

    add report generator

--- main.py
+++ main.py
@@ -9,3 +9,25 @@ def average_finder():
     average = (sum(numbers))/choice
     print(f"The Average of two numbers is {average}")
     i = None
+
+def students_report_card_generator():
+    choice = int(input("Enter how many sujects you want to enter: "))
+    subjects = []
+    subjects_marks = []
+    for i in range(choice):
+        subject = input("Enter subject name: ")
+        subject_mark = int(input("Enter marks in this subject: "))
+        subjects.append(subject)
+        subjects_marks.append(subject_mark)
+    # print(len(subjects))
+    # percentage = (sum(subjects_marks))/choice
+    # print(percentage)
+    len_of_subject = len(subjects)
+    a = 1
+    
+    print("S.no     Subject     Marks")
+    for i in range(len_of_subject):
+        print(f"{a}.        {subjects[i]}           {subjects_marks[i]}")
+        a += 1
+
+students_report_card_generator()
--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-mylist = [1 , 2 , 3 , 4]
-print(sum(mylist))
+# mylist = [1 , 2 , 3 , 4]
+# print(mylist[3])
+# print(len(mylist))
+
+
+for i in range(3):
+    a = 1
+    print(a)
+    a+=1
